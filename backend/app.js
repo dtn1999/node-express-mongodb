90d7b1c7ed7c6285193100f8f85dcc7fea73872c
danyls <danyngongang@gmail.com>
2020-11-26 07:05:27 +0100

    add new models to the api

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import Product from './models/product.js';
+import Thing from './models/thing.js';
 
 mongoose.connect('mongodb+srv://danyls:danyls@cluster0.qlso7.mongodb.net/test?retryWrites=true&w=majority',
   {
@@ -35,22 +35,66 @@ app.use(bodyParser.json());
  * get route
  */
 
-app.use('/api/products', (req, res) => {
-  Product.find()
+app.use('/api/things', (req, res) => {
+  Thing.find()
     .then((things) => res.status(200).json(things))
     .catch((error) => res.status(400).json({ error }));
 });
 
 /**
- * handle post request on route /api/product
+ * get element by id
  */
-app.post('/api/product', (req, res) => {
+app.get('/api/things/:id', (req, res) => {
+  Thing.findOne({ _id: req.params.id })
+    .then((thing) => res.status(200).json(thing))
+    .catch((error) => res.status(404).json({ error }));
+});
+/**
+ * handle post request on route /api/Thing
+ */
+app.post('/api/thing', (req, res) => {
   delete req.body._id;
-  const thing = new Product({
+  const thing = new Thing({
     ...req.body,
   });
   thing.save()
     .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
     .catch((error) => res.status(400).json({ error }));
+});
+
+/**
+ * handle update
+ */
+app.put('/api/thing/:id', (req, res) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !' }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+/**
+ * handle deletion
+ */
+app.delete('/api/thing/:id', (req, res) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+/**
+ * ========================= Handle stuff about products =======================
+ */
+
+/**
+  * get all products
+  */
+app.get('/api/products', (req, res) => {
+
+});
+
+/**
+  *
+  */
+app.get('/api/products', (req, res) => {
+
 });
 export default app;
--- backend/models/thing.js
+++ backend/models/thing.js
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose';
 
 const productSchema = mongoose.Schema({
-  name: { type: String, required: true },
+  title: { type: String, required: true },
   description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  userId: { type: String, required: true },
   price: { type: Number, required: true },
-  inStock: { type: Boolean, required: true },
 });
 
 export default mongoose.model('Product', productSchema);
